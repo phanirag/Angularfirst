e1db7d628427102dc7bea257d3b8cb4a01f48fa6
phanirag <raghavs025@gmail.com>
2018-09-09 17:32:10 +0530

    Changes are the concepts of Interpolation which is binding data of class to html

--- firstjs/src/app/test/test.component.ts
+++ firstjs/src/app/test/test.component.ts
@@ -4,14 +4,25 @@ import { Component, OnInit } from '@angular/core';
   // selector: 'app-test',
   //selector: '.app-test',
   selector: '[app-test]',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `<h2> This is from Test Component and name is {{name}} </h2>
+  <h2>{{2+2}}</h2>
+  <h2>{{name.length}}</h2>
+  <h2>{{name.toUpperCase()}}</h2>
+  <h2>{{2+2}}</h2>
+  <h2>{{conca()}}</h2>
+  <h2>{{siteurl}}</h2>`,
+  styles: []
 })
 export class TestComponent implements OnInit {
 
+  public name = "PhaniRaghava"
+  public siteurl= window.location.href;
   constructor() { }
 
   ngOnInit() {
   }
 
+  conca(){
+    return "hello" + this.name;
+  }
 }
